a6dffd2f8a86f8da4ed31d6b229ef3d346883d74
kuang.zukai <345106552@qq.com>
2017-01-14 10:49:45 +0800

    fix the copy-to-stepbystep script

--- copy-to-stepbystep.sh
+++ copy-to-stepbystep.sh
@@ -8,4 +8,13 @@ cp -rvf $myblog/db/* $stepbystep/db/
 cp -rvf $myblog/w3c/css/* $stepbystep/w3c/css/
 cp -rvf $myblog/w3c/duoshuo/* $stepbystep/w3c/duoshuo/
 cp -rvf $myblog/w3c/js/* $stepbystep/w3c/js/
-cp -rvf $myblog/_config.yml $stepbystep/
+
+# only base files in the directory
+cp -vf  $myblog/w3c/images/* $stepbystep/w3c/images/
+
+cp -vf  $myblog/index.html $stepbystep/
+cp -vf  $myblog/article.html $stepbystep/
+cp -vf  $myblog/404.html $stepbystep/
+
+# do not copy the _config.yml cause it has the settings of myblog
+# cp -rvf $myblog/_config.yml $stepbystep/
